b52d807b2055a0153881b7be3c13cf678abce681
otmazka <svetlankas@hot.ee>
2021-09-21 14:46:45 +0300

    second commit

--- src/sptv20game/SPTV20Game.java
+++ src/sptv20game/SPTV20Game.java
@@ -18,20 +18,28 @@ public class SPTV20Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // программа загадывает число и просит пользователя отгадать
+        //программа загадывает число и просит пользователя отгадать
         //если пользователь угадал, он выйграл
         //иначе пользователь проиграл
-        int max = 5, min = 0;
+        int max = 5, min = 0, attempt= 1;
         Random random = new Random();
         int gameNum = random.nextInt (max - min +1) + min;
         System.out.println("Программа загадала число в диапозоне от 0 до 5, угадай : ");
-        Scanner scanner = new Scanner(System.in);
-        int userNum = scanner.nextInt();
-        if(gameNum == userNum){
-            System.out.println("Ты выйграл");
-        }else{
-            System.out.println(":(ты проиграл. Задуманное число: " + gameNum);
+        for(int i=0;i<3; i++){
+            Scanner scanner = new Scanner(System.in);
+            int userNum = scanner.nextInt();
+            if(gameNum == userNum){
+                System.out.println("Ты выйграл");
+                break;
+            }else{
+                if(attempt < 3 ){
+                    System.out.println("Неправильно, попробуй еще раз: ");
+                }else{
+                    System.out.println("(ты проиграл. Задуманное число: " +gameNum);
+                }
+            }
+            attempt++;
+    
         }
     }
-    
 }
